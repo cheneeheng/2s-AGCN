a69797331f718a38d2280b685952344f3abbb2e8
chen <eeheng.chen@tum.de>
2022-03-29 16:04:39 +0200

    Code changes:
    - added aspp after SMP
    - added parts and motion as input

--- model/sgn_v2.py
+++ model/sgn_v2.py
@@ -6,9 +6,13 @@
 
 # Code refractored
 
-from torch import nn
 import torch
+from torch import nn
+from torch.nn import functional as F
+
 import math
+import copy
+from collections import OrderedDict
 
 
 class Module(nn.Module):
@@ -17,12 +21,14 @@ class Module(nn.Module):
                  out_channels: int,
                  kernel_size: int = 1,
                  padding: int = 0,
+                 dilation: int = 1,
                  bias: bool = False):
         super(Module, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.padding = padding
         self.kernel_size = kernel_size
+        self.padding = padding
+        self.dilation = dilation
         self.bias = bias
 
 
@@ -33,13 +39,55 @@ class SGN(nn.Module):
     c3 = 256  # gcn
     c4 = 512  # final conv
 
+    # NTU
+    parts_3points = [
+        (1, 0, 16),
+        (1, 0, 12),
+        (16, 0, 12),
+        (20, 1, 0),
+        (3, 2, 20),
+
+        (20, 4, 5),
+        (4, 5, 6),
+        (5, 6, 7),
+        (5, 6, 22),
+        (6, 7, 21),
+
+        (20, 8, 9),
+        (8, 9, 10),
+        (9, 10, 11),
+        (9, 10, 24),
+        (10, 11, 23),
+
+        (0, 12, 13),
+        (12, 13, 14),
+        (13, 14, 15),
+
+        (0, 16, 17),
+        (16, 17, 18),
+        (17, 18, 19),
+
+        (2, 20, 1),
+        (2, 20, 8),
+        (2, 20, 4),
+
+        (8, 20, 4),
+        (1, 20, 8),
+        (1, 20, 4),
+    ]
+
+    # 0,1 indicator of person 1 and 2 (dataloader)
+
     def __init__(self,
                  num_class: int = 60,
                  num_point: int = 25,
                  in_channels: int = 3,
                  seg: int = 20,
                  bias: bool = True,
-                 g_proj_shared: bool = False):
+                 g_proj_shared: bool = False,
+                 part: bool = False,
+                 motion: bool = False,
+                 aspp: list = None):
         super(SGN, self).__init__()
 
         self.num_class = num_class
@@ -48,6 +96,8 @@ class SGN(nn.Module):
         self.seg = seg
         self.bias = bias
         self.g_proj_shared = g_proj_shared
+        self.part = part
+        self.motion = motion
 
         self.pos_embed = embed(in_channels,
                                self.c1,
@@ -62,27 +112,62 @@ class SGN(nn.Module):
                                norm=True,
                                bias=bias)
 
+        if self.part:
+            self.par_embed = embed(in_channels*3,
+                                   self.c1,
+                                   inter_channels=self.c1,
+                                   num_point=len(self.parts_3points),
+                                   norm=True,
+                                   bias=bias)
+            if self.motion:
+                self.mot_embed = embed(in_channels,
+                                       self.c1,
+                                       inter_channels=self.c1,
+                                       num_point=len(self.parts_3points),
+                                       norm=True,
+                                       bias=bias)
+
         self.spa = one_hot(num_point, seg, mode=0)
         self.tem = one_hot(seg, num_point, mode=1)
-
-        self.tem_embed = embed(seg,
-                               self.c3,
-                               inter_channels=self.c1,
-                               num_point=num_point,
-                               norm=False,
-                               bias=bias)
         self.spa_embed = embed(num_point,
                                self.c1,
                                inter_channels=self.c1,
                                num_point=num_point,
                                norm=False,
                                bias=bias)
+        self.tem_embed = embed(seg,
+                               self.c3,
+                               inter_channels=self.c1,
+                               num_point=num_point,
+                               norm=False,
+                               bias=bias)
 
-        self.compute_g1 = compute_g_spa(self.c2, self.c3, bias=bias,
+        if self.part:
+            self.tem = one_hot(seg, num_point+len(self.parts_3points), mode=1)
+            self.gro = one_hot(len(self.parts_3points), seg, mode=0)
+            self.gro_embed = embed(len(self.parts_3points),
+                                   self.c1,
+                                   inter_channels=self.c1,
+                                   num_point=len(self.parts_3points),
+                                   norm=False,
+                                   bias=bias)
+
+        self.compute_g1 = compute_g_spa(self.c2,
+                                        self.c3,
+                                        bias=bias,
                                         g_proj_shared=g_proj_shared)
         self.gcn1 = gcn_spa(self.c2, self.c2, bias=bias)
         self.gcn2 = gcn_spa(self.c2, self.c3, bias=bias)
         self.gcn3 = gcn_spa(self.c3, self.c3, bias=bias)
+
+        if aspp is not None:
+            self.aspp = atrous_spatial_pyramid_pooling(
+                self.c3,
+                self.c3,
+                bias=bias,
+                dilations=aspp)
+        else:
+            self.aspp = lambda x: x
 
         self.smp = nn.AdaptiveMaxPool2d((1, seg))
         self.cnn = local(self.c3, self.c4, bias=bias)
@@ -90,6 +175,10 @@ class SGN(nn.Module):
         self.fc = nn.Linear(self.c4, num_class)
 
         self.init()
+
+        if self.part:
+            self.parts_3points = torch.tensor(self.parts_3points,
+                                              dtype=torch.int).reshape(-1)
 
     def init(self):
         for m in self.modules():
@@ -101,26 +190,53 @@ class SGN(nn.Module):
         nn.init.constant_(self.gcn2.w1.cnn.weight, 0)
         nn.init.constant_(self.gcn3.w1.cnn.weight, 0)
 
+    def pad_zeros(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.cat([x.new(*x.shape[:-1], 1).zero_(), x], dim=-1)
+
     def forward(self, x: torch.Tensor):
         bs, step, dim = x.shape
         assert dim % 3 == 0, "Only support input of xyz coordinates only."
 
         # Dynamic Representation
         num_point = dim // 3
-        x = x.view((bs, step, num_point, 3))  # n,t,v,c
-        x = x.permute(0, 3, 2, 1).contiguous()  # n,c,v,t
+        x1 = x.view((bs, step, num_point, 3))  # n,t,v,c
+        x = x1.permute(0, 3, 2, 1).contiguous()  # n,c,v,t
         dif = x[:, :, :, 1:] - x[:, :, :, 0:-1]  # n,c,v,t-1
-        dif = torch.cat([dif.new(*dif.shape[:-1], 1).zero_(), dif], dim=-1)
+        dif = self.pad_zeros(dif)
         pos = self.pos_embed(x)
         dif = self.vel_embed(dif)
-        dy = pos + dif  # n,c,v,t
+        dy1 = pos + dif  # n,c,v,t
+
+        if self.part:
+            par = torch.index_select(x1, 2, self.parts_3points)  # n,t,v+,c
+            par = par.view((bs, step, -1, 3, self.in_channels))  # n,t,v+,3,c
+            mid = par.mean(dim=-2, keepdim=True)  # n,t,v+,1,c
+            par = par - mid  # n,t,v+,3,c
+            par = par.view((bs, step, -1, self.in_channels*3))  # n,t,v+,c+
+            par = par.permute(0, 3, 2, 1).contiguous()  # n,c+,v+,t
+            dy2 = self.par_embed(par)  # n,c,v+,t
+
+            if self.motion:
+                mid = mid.squeeze(-2)  # n,t,v+,c
+                mid = mid.permute(0, 3, 2, 1).contiguous()  # n,c,v+,t
+                mot = mid[:, :, :, 1:] - mid[:, :, :, 0:-1]  # n,c,v+,t-1
+                mot = self.pad_zeros(mot)
+                mot = self.mot_embed(mot)
+                dy2 += mot
 
         # Joint and frame embeddings
         tem1 = self.tem_embed(self.tem(bs))
         spa1 = self.spa_embed(self.spa(bs))
+        if self.part:
+            gro1 = self.gro_embed(self.gro(bs))
 
         # Joint-level Module
-        x = torch.cat([dy, spa1], 1)  # n,c,v,t
+        if self.part:
+            x1 = torch.cat([dy1, spa1], 1)  # n,c,v,t
+            x2 = torch.cat([dy2, gro1], 1)  # n,c,v',t
+            x = torch.cat([x1, x2], 2)  # n,c,v'+v,t
+        else:
+            x = torch.cat([dy1, spa1], 1)  # n,c,v,t
         g = self.compute_g1(x)
         x = self.gcn1(x, g)
         x = self.gcn2(x, g)
@@ -129,6 +245,7 @@ class SGN(nn.Module):
         # Frame-level Module
         x = x + tem1
         x = self.smp(x)
+        x = self.aspp(x)
         x = self.cnn(x)
 
         # Classification
@@ -188,6 +305,7 @@ class embed(Module):
         self.cnn2 = cnn1x1(inter_channels, self.out_channels, bias=self.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: n,c,v,t
         x = self.norm(x)
         x = self.cnn1(x)
         x = self.relu(x)
@@ -201,10 +319,12 @@ class cnn1xn(Module):
         super(cnn1xn, self).__init__(*args, **kwargs)
         assert isinstance(self.kernel_size, int)
         assert isinstance(self.padding, int)
+        assert isinstance(self.dilation, int)
         self.cnn = nn.Conv2d(self.in_channels,
                              self.out_channels,
                              kernel_size=(1, self.kernel_size),
                              padding=(0, self.padding),
+                             dilation=self.dilation,
                              bias=self.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -216,6 +336,7 @@ class cnn1x1(cnn1xn):
     def __init__(self, *args, **kwargs):
         kwargs['kernel_size'] = 1
         kwargs['padding'] = 0
+        kwargs['dilation'] = 1
         super(cnn1x1, self).__init__(*args, **kwargs)
 
 
@@ -236,6 +357,7 @@ class local(Module):
         self.dropout = nn.Dropout2d(0.2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: n,c,v,t ; v=1 due to SMP
         x = self.cnn1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -285,7 +407,71 @@ class compute_g_spa(Module):
         return g4
 
 
+class atrous_spatial_pyramid_pooling(Module):
+    def __init__(self,
+                 *args,
+                 dilations: list = [1, 3, 5, 7],
+                 **kwargs):
+        super(atrous_spatial_pyramid_pooling, self).__init__(*args, **kwargs)
+        self.aspp = []
+
+        self.pool = 0 in dilations
+        if self.pool:
+            self.aspp.append(
+                nn.Sequential(
+                    OrderedDict([
+                        (f'avg_pool', nn.AdaptiveAvgPool2d(1)),
+                        (f'conv_pool', cnn1x1(self.in_channels,
+                                              self.out_channels,
+                                              bias=self.bias)),
+                        (f'relu_pool', nn.ReLU()),
+                    ])
+                )
+            )
+            dilations.pop(0)
+
+        for dil in dilations:
+            self.aspp.append(
+                nn.Sequential(
+                    OrderedDict([
+                        (f'conv_{dil}', cnn1xn(self.in_channels,
+                                               self.out_channels,
+                                               kernel_size=3,
+                                               padding=dil,
+                                               dilation=dil,
+                                               bias=self.bias)),
+                        (f'bn_{dil}', nn.BatchNorm2d(self.out_channels)),
+                        (f'relu_{dil}', nn.ReLU()),
+                    ])
+                )
+            )
+
+        branches = len(dilations) + 1 if self.pool else len(dilations)
+        self.proj = cnn1x1(self.out_channels * branches,
+                           self.out_channels,
+                           bias=self.bias)
+        self.bn = nn.BatchNorm2d(self.out_channels)
+        self.dropout = nn.Dropout2d(0.2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: n,c,v,t
+        res = []
+        for block in self.aspp:
+            res.append(block(x))
+        if self.pool:
+            res[0] = F.interpolate(res[0],
+                                   size=x.shape[-2:],
+                                   mode="bilinear",
+                                   align_corners=False)
+        res = torch.cat(res, dim=1)
+        x = self.proj(res)
+        x = self.bn(x)
+        x = self.dropout(x)
+        return x
+
+
 if __name__ == '__main__':
     batch_size = 2
-    model = SGN(seg=100)
-    model(torch.ones(batch_size, 100, 75))
+    model = SGN(seg=20, part=True, motion=True, aspp=[0, 1, 5, 9])
+    model(torch.ones(batch_size, 20, 75))
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
