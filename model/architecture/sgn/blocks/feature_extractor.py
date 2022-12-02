8cdf14db23dda68778c1d65a23338a06f0e86922
chen <eeheng.chen@tum.de>
2022-12-02 11:14:04 +0100

    added option to concat input emb

--- model/architecture/sgn/blocks/feature_extractor.py
+++ model/architecture/sgn/blocks/feature_extractor.py
@@ -17,6 +17,21 @@ SEGMENTS = [[2, 3],
             [7, 21, 22],
             [11, 23, 24]]
 
+PERIOD = 4
+
+
+def embedding_per_segment(x: Tensor, fn_list: torch.nn.ModuleList):
+    n, _, _, t = x.shape
+    x_segs = []
+    for idx, s in enumerate(SEGMENTS):
+        x_s = torch.stack([x[:, :, i, :] for i in s], 2)
+        x_s = torch.stack([x_s[:, :, :, i*PERIOD:(i+1)*PERIOD]
+                           for i in range(t//PERIOD)], 3)
+        x_s = x_s.permute((0, 4, 2, 1, 3))
+        x_s = x_s.reshape(n, -1, 1, x_s.shape[-1])
+        x_segs.append(fn_list[idx](x_s))
+    return torch.cat(x_segs, 2)  # n,c',v',t'
+
 
 class FeatureExtractor(PyTorchModule):
     def __init__(self,
@@ -24,10 +39,14 @@ class FeatureExtractor(PyTorchModule):
                  in_vel: int,
                  in_pos_emb_kwargs: dict,
                  in_vel_emb_kwargs: dict,
+                 fusion: int = 1
                  ):
         super(FeatureExtractor, self).__init__()
+
+        self.fusion = fusion
         self.in_pos = in_pos
         self.in_vel = in_vel
+
         if self.in_pos > 1000:
             self.pos_embed = torch.nn.ModuleList()
             for s in SEGMENTS:
@@ -38,6 +57,7 @@ class FeatureExtractor(PyTorchModule):
                 self.pos_embed.append(Embedding(**kwargs))
         elif self.in_pos > 0:
             self.pos_embed = Embedding(**in_pos_emb_kwargs)
+
         if self.in_vel > 1000:
             self.vel_embed = torch.nn.ModuleList()
             for s in SEGMENTS:
@@ -48,6 +68,7 @@ class FeatureExtractor(PyTorchModule):
                 self.vel_embed.append(Embedding(**kwargs))
         elif self.in_vel > 0:
             self.vel_embed = Embedding(**in_vel_emb_kwargs)
+
         if self.in_pos == 0 and self.in_vel == 0:
             raise ValueError("Input args are faulty...")
 
@@ -55,37 +76,28 @@ class FeatureExtractor(PyTorchModule):
         n, c, v, t = x.shape
         dif = x[:, :, :, 1:] - x[:, :, :, 0:-1]  # n,c,v,t-1
         dif = pad_zeros(dif)
-        if self.in_pos > 1000 and self.in_vel > 1000:
-            x_segs = []
-            for idx, s in enumerate(SEGMENTS):
-                x_s = torch.stack([x[:, :, i, :] for i in s], 2)
-                x_s = torch.stack([x_s[:, :, :, i*4:(i+1)*4]
-                                  for i in range(t//4)], 3)
-                x_s = x_s.permute((0, 4, 2, 1, 3))
-                x_s = x_s.reshape(n, -1, 1, x_s.shape[-1])
-                x_segs.append(self.pos_embed[idx](x_s))
-            pos = torch.cat(x_segs, 2)  # n,c',v',t'
-            x_segs = []
-            for idx, s in enumerate(SEGMENTS):
-                x_s = torch.stack([dif[:, :, i, :] for i in s], 2)
-                x_s = torch.stack([x_s[:, :, :, i*4:(i+1)*4]
-                                  for i in range(t//4)], 3)
-                x_s = x_s.permute((0, 4, 2, 1, 3))
-                x_s = x_s.reshape(n, -1, 1, x_s.shape[-1])
-                x_segs.append(self.vel_embed[idx](x_s))
-            vel = torch.cat(x_segs, 2)  # n,c',v',t'
-            dy1 = pos + vel  # n,c,v,t
+
+        if self.in_pos > 0 and self.in_vel > 0:
+            if self.in_pos > 1000 and self.in_vel > 1000:
+                pos = embedding_per_segment(x, self.pos_embed)
+                vel = embedding_per_segment(x, self.vel_embed)
+            else:
+                pos = self.pos_embed(x)
+                vel = self.vel_embed(dif)
+
+            if self.fusion == 0:
+                dy1 = torch.cat([pos, vel], dim=1)  # n,c,v,t
+            elif self.fusion == 1:
+                dy1 = pos + vel  # n,c,v,t
             return dy1, pos, vel
-        elif self.in_pos > 0 and self.in_vel > 0:
-            pos = self.pos_embed(x)
-            vel = self.vel_embed(dif)
-            dy1 = pos + vel  # n,c,v,t
-            return dy1, pos, vel
+
         elif self.in_pos > 0:
             dy1 = self.pos_embed(x)
             return dy1, dy1, None
+
         elif self.in_vel > 0:
             dy1 = self.vel_embed(dif)
             return dy1, None, dy1
+
         else:
             return None, None, None
--- model/architecture/sgn/sgn_v15.py
+++ model/architecture/sgn/sgn_v15.py
@@ -70,6 +70,7 @@ class SGN(PyTorchModule):
                  semantic_frame: int = 1,
                  semantic_class: int = 0,
 
+                 input_emb_fusion: int = 1,
                  semantic_joint_fusion: int = 0,
                  semantic_frame_fusion: int = 1,
                  semantic_frame_location: int = 0,
@@ -122,6 +123,8 @@ class SGN(PyTorchModule):
             raise ValueError("input_position is 0 but semantic_joint is not")
 
         # 0: concat, 1: sum
+        self.input_emb_fusion = input_emb_fusion
+        # 0: concat, 1: sum
         self.semantic_joint_fusion = semantic_joint_fusion
         # 0: concat, 1: sum
         self.semantic_frame_fusion = semantic_frame_fusion  # UNUSED
@@ -154,7 +157,8 @@ class SGN(PyTorchModule):
                 in_norm=self.normalization_fn_1d,
                 num_point=self.num_point,
                 mode=self.input_velocity,
-            )
+            ),
+            fusion=self.input_emb_fusion
         )
 
         # Semantic Embeddings --------------------------------------------------
@@ -507,63 +511,64 @@ if __name__ == '__main__':
     inputs = torch.ones(batch_size, 20, 75)
     # subjects = torch.ones(batch_size, 40, 1)
 
-    # base_path = "/code/2s-AGCN/data/data/ntu_result/xview/sgn/sgn_v15"
+    base_path = "/code/2s-AGCN/data/data/ntu_result/xview/sgn/sgn_v15"
     # file_path = "/221031120001_1head_256hdim_256out_nov_res_8head_16hdim_bn_rerun"  # noqa
-    # parser = get_parser()
-    # parser.set_defaults(**{'config': base_path + file_path + "/config.yaml"})
-    # args = load_parser_args_from_config(parser)
-    # model = SGN(**args.model_args)
-    # model(inputs)
-    # print(model)
-
-    model = SGN(
-        num_class=60,
-        num_point=8,
-        num_segment=5,
-        in_channels=3,
-        bias=1,
-        dropout=0.0,  # classifier
-        dropout2d=0.2,  # the rest
-        c_multiplier=[1.0, 1.0, 1.0, 1.0],
-        norm_type='bn',
-        act_type='relu',
-        input_position=1001,
-        input_velocity=1001,
-        semantic_joint=1,
-        semantic_frame=1,
-        semantic_joint_fusion=0,
-        semantic_frame_fusion=1,
-        semantic_frame_location=0,
-        spatial_mha_kwargs={
-            'd_model': [128],
-            'nhead': [1],
-            'd_head': [256],
-            'd_out': [256],
-            # 'v_proj': False,
-            # 'res_proj': True,
-            'dim_feedforward': [256],
-            'dim_feedforward_output': [256],
-            'dropout': 0.1,
-            'activation': "relu",
-            'num_layers': 1,
-            'norm': 'bn',
-            'global_norm': False
-        },
-        temporal_mha_kwargs={
-            'd_model': [256],
-            'nhead': [8],
-            'd_head': [16],
-            'dim_feedforward': [256],
-            'dim_feedforward_output': [512],
-            'dropout': 0.1,
-            'activation': "relu",
-            'num_layers': 1,
-            'norm': 'bn',
-            'global_norm': False
-        },
-    )
+    file_path = "/221124163001_8head_16hdim_128ffn_256out_8head_32hdim_256ffn_512out_qkv"  # noqa
+    parser = get_parser()
+    parser.set_defaults(**{'config': base_path + file_path + "/config.yaml"})
+    args = load_parser_args_from_config(parser)
+    model = SGN(**args.model_args)
     model(inputs)
     print(model)
+
+    # model = SGN(
+    #     num_class=60,
+    #     num_point=8,
+    #     num_segment=5,
+    #     in_channels=3,
+    #     bias=1,
+    #     dropout=0.0,  # classifier
+    #     dropout2d=0.2,  # the rest
+    #     c_multiplier=[1.0, 1.0, 1.0, 1.0],
+    #     norm_type='bn',
+    #     act_type='relu',
+    #     input_position=1001,
+    #     input_velocity=1001,
+    #     semantic_joint=1,
+    #     semantic_frame=1,
+    #     semantic_joint_fusion=0,
+    #     semantic_frame_fusion=1,
+    #     semantic_frame_location=0,
+    #     spatial_mha_kwargs={
+    #         'd_model': [128],
+    #         'nhead': [1],
+    #         'd_head': [256],
+    #         'd_out': [256],
+    #         # 'v_proj': False,
+    #         # 'res_proj': True,
+    #         'dim_feedforward': [256],
+    #         'dim_feedforward_output': [256],
+    #         'dropout': 0.1,
+    #         'activation': "relu",
+    #         'num_layers': 1,
+    #         'norm': 'bn',
+    #         'global_norm': False
+    #     },
+    #     temporal_mha_kwargs={
+    #         'd_model': [256],
+    #         'nhead': [8],
+    #         'd_head': [16],
+    #         'dim_feedforward': [256],
+    #         'dim_feedforward_output': [512],
+    #         'dropout': 0.1,
+    #         'activation': "relu",
+    #         'num_layers': 1,
+    #         'norm': 'bn',
+    #         'global_norm': False
+    #     },
+    # )
+    # model(inputs)
+    # print(model)
 
     for m in model.parameters():
         m.requires_grad = False
